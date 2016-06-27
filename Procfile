35b8ef37429149657dac1aebdc7e077882a5c488
Matthew Tardiff <mattrix@gmail.com>
2016-06-27 03:09:17 -0400

    Experimental Heroku support.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn coffeebar.app:app --log-file -
--- coffeebar/__main__.py
+++ coffeebar/__main__.py
@@ -4,4 +4,7 @@ from coffeebar.app import app
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('FLASK_HOST', '127.0.0.1'), debug=os.environ.get('FLASK_DEBUG', None) == 'True')
+    app.run(
+        host=os.environ.get('HOST', '127.0.0.1'),
+        port=os.environ.get('PORT', 5000),
+        debug=os.environ.get('DEBUG', None) == 'True')
